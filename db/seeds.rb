d3cc5c4b9129396da302fbf59271d4a60eedcd9d
Calum Gunn <calum@calumgunn.com>
2014-02-19 15:16:32 +0000

    README update and rake db:seeds to set up dev env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+Person.create(name: 'Anna')
+Person.create(name: 'Brendan')
+Person.create(name: 'Calum')
+Person.create(name: 'Erin')
+Person.create(name: 'Jamie')
+Person.create(name: 'Jenifer')
+Person.create(name: 'Joseph')
+Person.create(name: 'Mathew')
+Person.create(name: 'Tom')
+
+Theme.create(title: 'Dummy Theme', issue_number: '01', published: true)
+
+Link.create(
+  title: 'Fake Link 01', 
+  url: 'http://www.withassociates.com', 
+  description: 'Dummy link for your development environment.',
+  theme_id: 1,
+  person_id: 1
+)
+
+Link.create(
+  title: 'Fake Link 02',
+  url: 'http://www.withassociates.com',
+  description: 'Dummy link for your development environment.',
+  theme_id: 1,
+  person_id: 2
+)
