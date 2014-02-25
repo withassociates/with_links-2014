b4499604aca79e7082f394ca303dd801c7c130f9
Calum Gunn <calum@calumgunn.com>
2014-02-25 16:51:50 +0000

    Icons link to url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,19 +7,19 @@ module ApplicationHelper
     end
   end
 
-  def icon_or_away_icon(link)
-    if link.is_away?
-      'suitcase.png'
-    else
-      link.icon(:thumb)
-    end
-  end
-
   def icon_credit(link)
     if link.is_away?
       "Suitcase by Diego Naive from the Noun Project"
     else
       link.icon_attribution
+    end
+  end
+
+  def icon_link_unless_away(link)
+    if link.is_away?
+      image_tag 'suitcase.png'
+    else
+      link_to image_tag(link.icon(:thumb), width: 100), link.url
     end
   end
 
