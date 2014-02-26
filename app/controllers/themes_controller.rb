ebfdbeb17066e231e20a04cdfaeec85c4c27ef3a
Calum Gunn <calum@calumgunn.com>
2014-02-26 10:36:16 +0000

    Refactor themes controller methods

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,17 +1,13 @@
 class ThemesController < ApplicationController
-  def all
-    @themes = Theme.published.order('issue_date DESC')
+  def index
+    @themes = Theme.published
   end
 
   def show
     @theme = Theme.friendly.find(params[:id])
   end
 
-  def latest
-    @theme = Theme.published.last
-  end
-
-  def index
-    @theme = Theme.published.last
+  def home
+    @theme = Theme.published.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 WithLinksV2::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   get "pages/about", as: "about"
-  get "links/top", as: "top"
-  post "links/vote/:id", to: 'links#vote', as: 'vote'
-  get "themes/all", as: "all_themes"
+
+  # get "links/top", as: "top"
+  # post "links/vote/:id", to: 'links#vote', as: 'vote'
+
   resources :themes, :links, :people
 
-
-  root 'themes#index'
+  root 'themes#home'
 end
