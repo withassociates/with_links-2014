69885403743fa8a8c9e0510c9f2fb5b7c09946b7
Calum Gunn <calum@calumgunn.com>
2014-02-28 12:13:22 +0000

    Before filter redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_if_old_url
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def redirect_if_old_url
+    if request.host == 'withlinks.com'
+      redirect_to "http://links.withassociates.com#{request.request_uri}", status: :moved_permanently
+    end
+  end
 end
