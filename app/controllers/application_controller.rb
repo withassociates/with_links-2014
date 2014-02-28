dc754d8058933b990e262e47e7eff28dc2c28d01
Calum Gunn <calum@calumgunn.com>
2014-02-28 15:48:31 +0000

    Redirect if double dub dubs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def redirect_if_old_url
-    if request.host == 'withlinks.com'
+    if request.host == 'withlinks.com' || request.host == 'www.withlinks.com'
       redirect_to "http://links.withassociates.com#{request.fullpath}", status: :moved_permanently
     end
   end
