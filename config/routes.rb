d460bba1eeba7a1816affc471581492802031493
Erin Staniland <erin@withassociates.com>
2014-12-16 22:21:01 +0000

    Remove /pages/about url

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ WithLinksV2::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get "pages/about", as: "about"
-
   get '/latest-issue', to: 'themes#email', as: 'latest_issue'
 
   resources :themes, :links, :people, only: [:show, :index]
